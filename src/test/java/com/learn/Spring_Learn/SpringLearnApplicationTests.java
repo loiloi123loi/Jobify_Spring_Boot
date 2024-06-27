179548f9479dee1a4063f4e8baee1ea50341cc58
Lợi <loivantran10012002@gmail.com>
2024-06-27 16:55:29 +0700

    create project

---
+++ src/test/java/com/learn/Spring_Learn/SpringLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.Spring_Learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
